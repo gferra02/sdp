c1107809ddaa61f69464399d4a800e32ff59191f
gferra02 <gferra02@mail.bbk.ac.uk>
2018-01-25 19:10:54 +0000

    Examples from class (week-3)

--- exercises/three/person/src/types/sum.kt
+++ exercises/three/person/src/types/sum.kt
@@ -5,7 +5,20 @@ package types
 
 // if A is a B or a C
 
-open class AA()
-class BB() : AA()
-class CC() : AA()
+//open class AA()
+//class BB() : AA()
+//class CC() : AA()
+
+//// In Kotlin 1.2 should be able to do this:
+//sealed class AA()
+//data class BB() : AA()
+//data class CC() : AA()
+
+sealed class AA() {
+    // Defining all the subtypes here, can't extend outside these subclasses
+    class BB() : AA()
+    class CC() : AA()
+}
+
+// Sum type pattern
 
