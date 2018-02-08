5552c8d5a510a2f77c0d091b98ce545dd6d477b2
gferra02 <gferra02@mail.bbk.ac.uk>
2018-02-08 18:58:36 +0000

    Ex one, ws two - Done

--- exercises/01/worksheet-two-reflection-gferra02-master/exercise-one/src/Reflect.kt
+++ exercises/01/worksheet-two-reflection-gferra02-master/exercise-one/src/Reflect.kt
@@ -1,16 +1,19 @@
+import java.io.File
+import java.util.*
+
 fun main(args: Array<String>) {
     if (args.size != 1) {
         println("Requires a class name as argument to program")
         System.exit(-1)
     }
 
-    // TODO()
-
-    read("aClassFile")
-
+    System.out.println("What file do you want me to read?\n(Type the name of the file without the extension): ")
+    val thisFile: String? = readLine()
+    if (thisFile == "") println("No file then? OK")
+    else read(thisFile)
 }
 
-fun read(fileName: String) {
+fun read(fileName: String?) {
     val theName = Class.forName(fileName)
     println(theName)
 }
